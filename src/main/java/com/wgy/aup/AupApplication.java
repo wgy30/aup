96061cc433f7f6a1651ece741d71627eb144573f
wgy30 <960157662@qq.com>
2021-08-08 02:38:41 +0800

    项目搭建

---
+++ src/main/java/com/wgy/aup/AupApplication.java
@@ -0,0 +1,13 @@
+package com.wgy.aup;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AupApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AupApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/wgy/aup/AupApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wgy.aup;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AupApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
