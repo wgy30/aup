3e07aeb6d00c3dd089ab068d8573d37c08db8f15
wgy30 <960157662@qq.com>
2021-08-15 23:11:06 +0800

    no message

--- src/main/java/com/wgy/aup/common/core/context/Constants.java
+++ src/main/java/com/wgy/aup/common/core/context/Constants.java
@@ -6,9 +6,9 @@ package com.wgy.aup.common.core.context;
  * version 2021/8/8 17:51:23
  */
 public class Constants {
-    public static final String KEY_CURRENT_NAME = "currentName"; //当前用户名
-    public static final String KEY_CURRENT_CODE = "currentCode"; //当前用户编码
-    public static final String KEY_CURRENT_NUMBER = "number";   //工号/学号
+    public static final String KEY_CURRENT_NAME = "currentName";
+    public static final String KEY_CURRENT_CODE = "currentCode";
+    public static final String KEY_CURRENT_NUMBER = "number";
 
 
     public static final String KEY_TOKEN_NAME = "token";
