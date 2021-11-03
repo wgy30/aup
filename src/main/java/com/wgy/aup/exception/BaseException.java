c0efd8b82631f3a2fcdb7615f3e7b99b7ed658cc
wgy30 <960157662@qq.com>
2021-11-04 01:31:29 +0800

    异常类设计

---
+++ src/main/java/com/wgy/aup/exception/BaseException.java
@@ -0,0 +1,43 @@
+package com.wgy.aup.exception;
+
+/**
+ * @author wgy
+ * @version 2021/10/4 15:53:55
+ */
+public class BaseException extends RuntimeException {
+
+    private String message;
+
+    private Integer code;
+
+    BaseException() {
+        super();
+    }
+
+    BaseException(String message) {
+        super(message);
+        this.message = message;
+    }
+
+    BaseException(Integer code, String message) {
+        super(message);
+        this.message = message;
+        this.code = code;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public String getMessage() {
+        return this.message;
+    }
+
+    public void setCode(Integer code) {
+        this.code = code;
+    }
+
+    public Integer getCode() {
+        return code;
+    }
+}
---
+++ src/main/java/com/wgy/aup/exception/BasicServiceException.java
@@ -0,0 +1,12 @@
+package com.wgy.aup.exception;
+
+/**
+ * @author wgy
+ * @version 2021/10/4 16:02:05
+ */
+public class BasicServiceException extends BaseException {
+
+    public BasicServiceException(String message) {
+        super(message);
+    }
+}
